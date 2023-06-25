16374b0bf9f5fd0ad24db5e3d4c2980f7758f4be
yaoweijian <rubygba@foxmail.com>
2023-06-25 10:43:00 +0800

    feat: esm test

---
+++ public/test/index.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: yaoweijian rubygba@foxmail.com
+ * @Date: 2023-06-25 10:27:38
+ * @LastEditors: yaoweijian rubygba@foxmail.com
+ * @LastEditTime: 2023-06-25 10:41:45
+ * @FilePath: /little-pack/public/test/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { myAll } from './promise.js'
+
+console.log('myAll', myAll)
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('p1 res')
+  }, 100)
+})
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('p2 res')
+  }, 1000)
+})
+
+console.log(Promise.all([p1, p2]))
+// console.log(myAll([p1, p2]))
+console.log(myAll([p1, p2])
+  .then(res => console.log(res))
+  .catch(e => console.error(e)))
--- public/test/promise.js
+++ public/test/promise.js
@@ -2,11 +2,11 @@
  * @Author: yaoweijian rubygba@foxmail.com
  * @Date: 2023-06-23 09:15:21
  * @LastEditors: yaoweijian rubygba@foxmail.com
- * @LastEditTime: 2023-06-25 10:24:47
+ * @LastEditTime: 2023-06-25 10:35:56
  * @FilePath: /little-pack/public/test/promise.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-function myAll (arr) {
+export function myAll (arr) {
   return new Promise ((resolve, reject) => {
     if (!Array.isArray(arr)) return reject(new Error('传入参数必须是数组'))
 
@@ -24,5 +24,4 @@ function myAll (arr) {
     //   return resolve(result)
     // }
   }).catch(e => reject(e))
-  
 }
