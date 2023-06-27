4cfd19d327edd7161ba282ea908615d519a3f390
yaoweijian <rubygba@foxmail.com>
2023-06-27 14:14:37 +0800

    feat: add lazyMan class test

---
+++ public/test/hook.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: yaoweijian rubygba@foxmail.com
+ * @Date: 2023-06-27 13:38:58
+ * @LastEditors: yaoweijian rubygba@foxmail.com
+ * @LastEditTime: 2023-06-27 14:14:16
+ * @FilePath: /little-pack/public/test/hook.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export class lazyMan {
+  constructor(name = '') {
+    this.stack = []
+    console.log(`my name is ${name}`)
+    setTimeout(() => {
+      this.next()
+    }, 0)
+  }
+
+  next() {
+    const work = this.stack.shift()
+    work && work()
+  }
+
+  sleep(time = 1000) {
+    this.stack.push(() => {
+      setTimeout(() => {
+        console.log(`sleep over ${time}`)
+        this.next()
+      }, time)
+    })
+    // this.next()
+    return this
+  }
+
+  sleepBefore(time = 1000) {
+    this.stack.unshift(() => {
+      setTimeout(() => {
+        console.log(`sleep over ${time} (before)`)
+        this.next()
+      }, time)
+    })
+    // this.next()
+    return this
+  }
+}
--- public/test/index.js
+++ public/test/index.js
@@ -2,7 +2,7 @@
  * @Author: yaoweijian rubygba@foxmail.com
  * @Date: 2023-06-25 10:27:38
  * @LastEditors: yaoweijian rubygba@foxmail.com
- * @LastEditTime: 2023-06-25 12:29:04
+ * @LastEditTime: 2023-06-27 14:04:59
  * @FilePath: /little-pack/public/test/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,3 +28,6 @@ console.log('allRes', allRes)
 console.log(myAll([p1, p2])
   .then(res => console.log(res))
   .catch(e => console.error(e)))
+
+import { lazyMan } from './hook.js'
+const a = new lazyMan('aaa').sleep(100).sleepBefore(500)
